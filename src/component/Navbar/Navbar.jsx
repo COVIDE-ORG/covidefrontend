3e9f9a96c7ed02a7a51d34ddc71fc0c5d598adf1
Abhishek Raj <abhishekraj272@gmail.com>
2021-05-06 13:21:31 +0530

    ADDED: Navbar

---
+++ src/component/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Navbar.css";
+
+export default function Navbar() {
+    return (
+        <div className="navbar">
+            <p>COVID CROWDSOURCED DATA</p>
+            
+            <a href="https://twitter.com/hashtag/IndiaFightsCorona">
+                <p>#IndiaFightsCorona</p>
+            </a>
+        </div>
+    )
+}
