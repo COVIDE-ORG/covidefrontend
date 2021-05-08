8efddb5cdc78e72e6e6cd34758ed9dcf4855fd1a
sambitos23 <sambit28saha@gmail.com>
2021-05-08 18:52:18 +0530

    Volunteer section added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Body from './component/Body/Body';
 import Navbar from './component/Navbar/Navbar';
+import Volunteer from './component/Volunteer/Volunteer';
 
 function App() {
   return (
     <div className="app">
       <Navbar />
-      <Body />
+      {/* <Body /> */}
+      <Volunteer/>
     </div>
   );
 }
---
+++ src/component/Volunteer/Volunteer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Volunteer() {
+    return (
+        <div>
+           <iframe style={{marginTop: '5%', overflow: 'auto'}} src="https://docs.google.com/forms/d/e/1FAIpQLSeGdEmBYZ48xB3lXDUrtqMwhv0azuMmL5QyPxU5wp6NMcgVPA/viewform?embedded=true" width="100%" height="1100" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+        </div>
+    )
+}
+
+export default Volunteer
