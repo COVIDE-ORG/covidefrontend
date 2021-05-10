8c2cfd127dcd74297f07a9c7eb11788784de4202
Abhishek Raj <abhishekraj272@gmail.com>
2021-05-10 12:53:47 +0530

    Added redux-thunk and actions
    Co-authored-by: Sakshi Gairola <grsakshi@users.noreply.github.com>

---
+++ src/constant/baseUrl.js
@@ -0,0 +1,3 @@
+const  BASE_URL = "http://168.62.36.33:8000/api/resource/";
+
+export default BASE_URL;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,16 @@
-import { createStore } from "redux";
+import { applyMiddleware, compose, createStore } from "redux";
 import reducer from "./reducer";
+
+import thunk from 'redux-thunk';
+
+const middlewares = [thunk];
 
 const store = createStore(
     reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    compose(
+        applyMiddleware(...middlewares),
+        window?.__REDUX_DEVTOOLS_EXTENSION__()
+    )
 )
 
 export default store;
