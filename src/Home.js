95979c7132b09ae8446d8b7ef2babc5db9cd5c41
2221mb <manish.manishbhardwaj007@gmail.com>
2021-05-23 12:18:15 +0530

    meta data

--- src/Home.js
+++ src/Home.js
@@ -38,7 +38,7 @@ const closeModal = () => {
         <title>Covid Resources</title>
         <meta
       name="description"
-      content="A comprehensive resource list of Oxygen, Ambulance Services, Plasma, Remdesivir, Tocilizumab Hospital Beds in India. COVID 19 India Help- View all verified resources, ranging from Pan-India to State-wise COVID-19 Resource Lists. Verified leads for Plasma, Ambulance, Oxygen, Remdesivir and Hospital Beds to help in COVID 19 emergencies in India."
+      content="Covid Resources. List of Oxygen, Ambulance Services, Plasma, Hospital Beds, Remdesivir, Tocilizumab, FabiFlu etc in India. COVID-19 India Help- View all resources, ranging from Pan-India to State-wise COVID-19 Resource Lists."
     />
     </Helmet>
       <Header/>
